da41f35bbb457c8fb623e4806e025983bf84199a
李步钻 <leepoo2008@163.com>
2019-05-09 14:18:34 +0800

    实现数据的显示和提交按钮的实现
    Signed-off-by: 李步钻 <leepoo2008@163.com>

--- src/TodoList.js
+++ src/TodoList.js
@@ -4,15 +4,41 @@ export default class TodoList extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            inputValue: 'hello'
+            inputValue: '', // 输入框的值
+            list: [], // 数组回显
         }
     }
     render() {
         return (
             <div>
-                <input value="{this.state.inputValue}" />
-                <button >提交</button>
+                <input value={this.state.inputValue} onChange={this.handleChange}/>
+                <button onClick={this.handleClick}>提交</button>
+                <ul>
+                   {
+                       this.state.list.map( (item, index) => {
+                           return (
+                                <li key={index}>
+                                     {item}
+                                </li>
+                           )
+                       })
+                   }
+                </ul>
             </div>
         )
+    }
+    handleClick = () => {
+        let list = [...this.state.list, this.state.inputValue];
+        this.setState({
+            list: list, // 设置更新数据实现视图更新
+            inputValue: '' // 每次输入完后清空输入框的值
+        })
+        
+    }
+    handleChange = (e) => {
+        this.setState({
+            inputValue: e.target.value
+        })
+        
     }
 }
